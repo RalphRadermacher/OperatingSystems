925edd1c4a409f6a1dcfc04b1cbbb75b9bf27fdd
Ralph Radermacher <ralph.radermacher@gmx.net>
2024-04-29 11:06:05 +0200

    Args and Translate

---
+++ src/12 Translate/translate.c
@@ -0,0 +1,75 @@
+/* -------------------------------------------------------------------------- */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* -------------------------------------------------------------------------- */
+
+char transChar (char src, char s1 [], char s2 [])
+{
+	int i;
+	int n = strlen (s1);
+	
+	for (i = 0; i < n; i++)
+	{
+		if (src == s1 [i])
+		{
+			return s2 [i];
+		}
+    }	
+    
+    return src;
+}
+
+/* -------------------------------------------------------------------------- */
+
+char* transString (char src [], char tgt [], char s1 [], char s2 [])
+{
+	int i;
+	int n = strlen (src);
+	
+	for (i = 0; i < n; i++)
+	{
+		tgt [i] = transChar (src [i], s1, s2);
+	}
+	
+	return tgt;
+}
+
+/* -------------------------------------------------------------------------- */
+
+int main (int argc, char* argv[])
+{
+    if (argc != 3)
+    {
+		fprintf (stderr, "USAGE: %s <source> <target>\n", argv [0]);
+		exit (1); 
+	}
+	
+	if (strlen (argv [1]) != strlen (argv [2]))
+	{
+		fprintf (stderr, "ERROR: <source> and <target> must be the same length\n");
+		exit (1); 
+	}
+	
+	// Copy args
+	
+	char* set1 = argv [1];
+	char* set2 = argv [2];
+	
+	char source [1024];
+	char target [1024];
+	
+	while (scanf ("%s", source) != EOF ) 
+	{ 
+        transString (source, target, set1, set2);
+        
+        printf ("%s\n", target);
+    }
+
+    return 0;
+}
+
+/* -------------------------------------------------------------------------- */
+
