2c9a3a3b62c08a9af4f96499261e5a84b5f7f78b
Ralph Radermacher <ralph.radermacher@gmx.net>
2024-05-22 12:47:34 +0200

    ProduceConsumer

--- src/10_ProducerConsumer/producer_consumer.c
+++ src/10_ProducerConsumer/producer_consumer.c
@@ -64,20 +64,92 @@ void putSynchronized (int bufElem)
 
 /* -------------------------------------------------------------------------- */
 
+int produce ()
+{
+    return rand () % 100;
+}
+
+/* -------------------------------------------------------------------------- */
+
+#define PRODUCERS  5
+#define CONSUMERS  3
+#define ITEMS      1
+
+#define ITEM_SUM  (PRODUCERS * CONSUMERS * ITEMS)
+
+#define PRODUCER_ITEMS ITEM_SUM / PRODUCERS
+#define CONSUMER_ITEMS ITEM_SUM / CONSUMERS
+
+void *producer (int *pnr)
+{
+    int item;
+    
+    for (int i = 0; i < PRODUCER_ITEMS; i++)
+    {
+        item = produce ();
+        printf ("producer [%d].put (%d)\n", *pnr, item);
+        putSynchronized (item);
+    }
+    
+    pthread_exit (0);
+}
+
+void *consumer (int *cnr)
+{
+    for (int i = 0; i < CONSUMER_ITEMS; i++)
+    {
+        printf ("consumer [%d].get () = %d\n", *cnr, getSynchronized ());
+    } 
+    
+    pthread_exit (0);
+}
+
+/* -------------------------------------------------------------------------- */
+
 int main (int argc, char *argv[]) 
 {
+    int       producers [PRODUCERS];
+    int       consumers [CONSUMERS];
+    
+    pthread_t pids      [PRODUCERS];
+    pthread_t cids      [CONSUMERS];
+    
+    int       i;
+    
+    for (i = 0; i < PRODUCERS; i++)
+    {
+        producers [i] = i;
+    }
+    
+    for (i = 0; i < CONSUMERS; i++)
+    {
+        consumers [i] = i;
+    }
+    
     sem_init (&mutex, 0, 1);
     sem_init (&empty, 0, 0);
     sem_init (&full,  0, BUF_SIZE);
 
-    putSynchronized (5);
-    putSynchronized (3);
-    putSynchronized (1);
+    for (i = 0; i < CONSUMERS; i++)
+    {
+        pthread_create (&cids [i], NULL, (void*) consumer, (void*) &consumers [i]);
+    }
     
-    printf ("-> %d\n", getSynchronized ());
-    printf ("-> %d\n", getSynchronized ());
-    printf ("-> %d\n", getSynchronized ());
+    for (i = 0; i < PRODUCERS; i++)
+    {
+        pthread_create (&pids [i], NULL, (void*) producer, (void*) &producers [i]);
+    }
     
+    for (i = 0; i < PRODUCERS; i++)
+    {
+        pthread_join (pids [i], NULL);
+    }
+    
+    for (i = 0; i < CONSUMERS; i++)
+    {
+        pthread_join (cids [i], NULL);
+    }
+        
     sem_destroy (&full);
     sem_destroy (&empty);
     sem_destroy (&full);
