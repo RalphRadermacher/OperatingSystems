ceccc847ec7c6321c31e421e20337396c5fbebcd
Ralph Radermacher <ralph.radermacher@gmx.net>
2024-12-10 18:26:56 +0100

    Update comments

--- src/04_Args/args.c
+++ src/04_Args/args.c
@@ -1,5 +1,18 @@
 /* -------------------------------------------------------------------------- */
 
+/*
+ * Vorlesung Betriebssysteme an der DHBW Karlsruhe und Mannheim
+ * 
+ * Dozent: Dr. Ralph Radermacher
+ * 
+ * Aufgabe - Argumente
+ * 
+ * Schreiben Sie ein C-Programm args.c, das die Argumente der Kommandozeile 
+ * in der Standausgabe ausgibt.
+ */
+ 
+/* -------------------------------------------------------------------------- */
+
 #include <stdio.h>
 
 /* -------------------------------------------------------------------------- */
