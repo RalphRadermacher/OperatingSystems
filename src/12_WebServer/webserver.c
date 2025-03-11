5ca5b1db5e0afaf40dd249f1b4672e01290a2e9d
Ralph Radermacher <ralph.radermacher@gmx.net>
2025-03-11 10:48:28 +0100

    getFileUrl ()

--- src/12_WebServer/webserver.c
+++ src/12_WebServer/webserver.c
@@ -9,6 +9,7 @@
 /* -------------------------------------------------------------------------- */
 
 #include <sys/socket.h>
+#include <netdb.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdio.h>
@@ -25,19 +26,91 @@
 
 /* -------------------------------------------------------------------------- */
 
+#define PORT_NUMBER 5000
+
+int port = PORT_NUMBER;
+
+/* -------------------------------------------------------------------------- */
+
 #define BUF_SIZE 5 
 #define WORKERS  5
 
-pthread_t wids [WORKERS];
-int       workers [WORKERS];
+pthread_t wIds [WORKERS]; // Ids of the worker threads
+int       wNrs [WORKERS]; // Nr of the worker threads
  
+/* -------------------------------------------------------------------------- */
+
+#define MIME_TYPE_DEFAULT "ext/html"
+
+char* mimeTypes [][2] = 
+{
+    {".html", "text/html"},
+    {".css",  "text/css"},
+    {".js",   "application/js"},
+    {".jpg",  "image/jpeg"},
+    {".png",  "image/png"},
+    {".gif",  "image/gif"},
+    {".html", "text/html"},
+    { NULL,    NULL}    
+};
+
+char* getMimeType (char *fileName)
+{
+    const char *dot = strrchr (fileName, '.');
+    
+    if (dot == NULL)
+    {
+        return MIME_TYPE_DEFAULT;
+    }
+    
+    for (int i = 0; mimeTypes [i][0] != NULL; i++)
+    {
+        if (strcmp (dot, mimeTypes [i][0]) == 0)
+        {
+            return mimeTypes [i][1];
+        }
+    }
+    
+    return  MIME_TYPE_DEFAULT;
+}
+
+/* -------------------------------------------------------------------------- */
+
+void getFileURL (char *route, char *fileURL)
+{
+      char *question = strrchr (route, '?');
+      
+      if (question)
+      {
+          *question = '\0';
+      }
+    
+      if (route [strlen (route) - 1] == '/')
+      {
+          strcat (route, "index.html");
+      }
+    
+      strcpy (fileURL, "htdocs");
+      strcat (fileURL, route);
+    
+      const char *dot = strrchr(fileURL, '.');
+      
+      if (!dot || dot == fileURL)
+      {
+          strcat (fileURL, ".html");
+      }
+}
+
 /* -------------------------------------------------------------------------- */
 
 void *worker (int *wnr)
 {
     int   connFd;
     int   nBytes = 0;
+    
     char  recvBuf [1024];
+    char  method  [32];
+    char  route   [1924];
      
     logI ("Worker %d started\n", *wnr);
     
@@ -47,9 +120,15 @@ void *worker (int *wnr)
         
         if ((nBytes = read (connFd, recvBuf, sizeof (recvBuf) - 1)) > 0)
         {
-            recvBuf [nBytes] = 0;
+            recvBuf [nBytes] = '\0';
         
-            logI ("Worker %d : %s\n", *wnr, recvBuf);
+            sscanf (recvBuf, "%s %s", method, route);
+
+            logI ("Worker %d : %s %s\n", *wnr, method, route);
+            
+            const char response[] = "HTTP/1.1 404 Not Found\r\n\n";
+            send (connFd, response, sizeof (response), 0);
+
         } 
 
         close (connFd);
@@ -66,14 +145,13 @@ int initialize ()
     
     for (i = 0; i < WORKERS; i++)
     {
-        workers [i] = i;
+        wNrs [i] = i;
     }
     
     for (i = 0; i < WORKERS; i++)
     {
-        logI ("create (%d)\n", workers [i]);
-        pthread_create (&wids [i], NULL, (void*) worker, (void*) &workers [i]);
-    }
+        pthread_create (&wIds [i], NULL, (void*) worker, (void*) &wNrs [i]);
+   }
     
     return 0;
 }
@@ -89,22 +167,38 @@ int main (int argc, char *argv[])
     initialize ();
     
     listenFd = socket (AF_INET, SOCK_STREAM, 0);
+    setsockopt (listenFd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int));
     
     memset (&servAddr, '0', sizeof (servAddr));
 
     servAddr.sin_family      = AF_INET;
-    servAddr.sin_addr.s_addr = htonl (INADDR_ANY);
-    servAddr.sin_port        = htons (5000); 
+    servAddr.sin_addr.s_addr = htonl (INADDR_LOOPBACK);
+    servAddr.sin_port        = htons (port); 
 
-    bind (listenFd, (struct sockaddr*) &servAddr, sizeof (servAddr)); 
-
+    bind   (listenFd, (struct sockaddr*) &servAddr, sizeof (servAddr)); 
     listen (listenFd, 10); 
+    
+    char hostBuffer    [NI_MAXHOST];
+    char serviceBuffer [NI_MAXSERV];
+    
+    getnameinfo 
+    (
+        (struct sockaddr *) &servAddr, 
+        sizeof (servAddr), 
+        hostBuffer,
+        sizeof (hostBuffer), 
+        serviceBuffer, 
+        sizeof (serviceBuffer), 
+        0
+     );
+    
+    logI ("%s is listening on http://%s:%s/\n", argv [0], hostBuffer, serviceBuffer);
     
     while (1)
     {
         connFd = accept (listenFd, (struct sockaddr*) NULL, NULL); 
         
-        logI ("Connection accepted on port %d\n", 5000);
+        logI ("Connection accepted on port %d\n", port);
         
         bufferPut (connFd);
      }
